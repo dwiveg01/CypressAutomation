59fac9556ed2770eb656ce1970eeb17dc5946264
Gaurav <gaurav.dwivedy94@gmail.com>
2022-12-01 12:26:14 +0530

    My 2nd Commit

--- cypress/Integration/examples/Test2.js
+++ cypress/Integration/examples/Test2.js
@@ -54,19 +54,6 @@ it('Test Case 3: Add the product to Cart as per name',()=>{
   
 })
 
-it('Test Case 5: Add the product to Cart as per name duplicate',()=>{
-
-  const homePage=new HomePage
-  const productPage=new ProductPage 
-  
-  homePage.getShopTab().click()
-  cy.selectProduct(loginData.productName)
-  
-  productPage.getCheckoutButton().click()
-  productPage.getCheckoutFinalButton().click()
-  cy.log("Test case passed.")
-  
-})
 
 
    })
